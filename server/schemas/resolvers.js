70e7c3b0d99484c6b1d75c760230d302a881b0c1
unknown <ommarkhawaja1@gmail.com>
2022-09-17 10:40:09 -0400

    single thought and profile page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,42 +1,42 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
 
         return userData;
       }
+
       throw new AuthenticationError('Not logged in');
     },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find().sort({ createdAt: -1 });
-    },
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
-    },
-    // get all users
     users: async () => {
       return User.find()
         .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
+        .populate('thoughts')
+        .populate('friends');
     },
-    // get a user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    }
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -101,7 +101,7 @@ const resolvers = {
 
       throw new AuthenticationError('You need to be logged in!');
     }
-  },
+  }
 };
 
 module.exports = resolvers;
